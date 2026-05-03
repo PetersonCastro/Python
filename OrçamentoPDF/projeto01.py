c7c818864a021ebdc19ad2228d6c8d04c378cea2
PetersonCastro <castropeterson485@gmail.com>
2026-05-03 00:07:06 -0300

    Atualização avançada dos modelos

--- OrçamentoPDF/projeto01.py
+++ OrçamentoPDF/projeto01.py
@@ -10,15 +10,15 @@ valor_total = int(horas_previstas)*int(valor_hora)
 pdf = FPDF()
 
 pdf.add_page()
-pdf.set_font("Arial")
+pdf.set_font("Arial", size=15, style="B")
 
-pdf.image(r"C:\Users\Pichau\OneDrive\Documentos\Peterson - Programação\OrçamentoPDF\template.png", x=0, y=0)
+pdf.image(r"C:\Users\Pichau\OneDrive\Documentos\Peterson - Programação\Python\OrçamentoPDF\template.png", x=0, y=0 , w=210, h=297)
 
-pdf.text(115, 145, projeto)
-pdf.text(115, 160, horas_previstas)
-pdf.text(115, 175, valor_hora)
-pdf.text(115, 190, prazo)
-pdf.text(115, 205, str(valor_total))
+pdf.text(115, 155, projeto.upper())
+pdf.text(115, 175, horas_previstas)
+pdf.text(115, 194, valor_hora)
+pdf.text(115, 213, prazo)
+pdf.text(115, 230, str(valor_total))
 
 pdf.output("Orçamento.pdf")
 print("Orçamento gerado com sucesso!!!")
