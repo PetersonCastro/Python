e54390c066777fbedbe7cb08ed390beba3b6cf87
PetersonCastro <castropeterson485@gmail.com>
2026-05-02 22:58:11 -0300

    Atualizando codigo

--- OrçamentoPDF/projeto01.py
+++ OrçamentoPDF/projeto01.py
@@ -12,7 +12,7 @@ pdf = FPDF()
 pdf.add_page()
 pdf.set_font("Arial")
 
-pdf.image(r"C:\Users\Pichau\OneDrive\Documentos\Peterson - Programação\Live 1\template.png", x=0, y=0)
+pdf.image(r"C:\Users\Pichau\OneDrive\Documentos\Peterson - Programação\OrçamentoPDF\template.png", x=0, y=0)
 
 pdf.text(115, 145, projeto)
 pdf.text(115, 160, horas_previstas)
